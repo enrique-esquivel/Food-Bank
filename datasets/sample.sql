3abe605df5b50de5dc2806474a774096813903b6
enrique-esquivel <enrique.esquivel@outlook.com>
2021-06-24 22:36:33 -0500

    Notes field for Delivery Item

--- datasets/sample.sql
+++ datasets/sample.sql
@@ -7,16 +7,18 @@ CREATE TABLE "Account" (
 );
 INSERT INTO "Account" VALUES(1,'Cuenta de ejemplo para asignaciones','');
 INSERT INTO "Account" VALUES(2,'Fresh Groceries Market','');
+INSERT INTO "Account" VALUES(3,'Cuenta de ejemplo para asignaciones','');
 CREATE TABLE "Delivery_Item__c" (
 	id INTEGER NOT NULL, 
 	"Food_Expiration_Date__c" VARCHAR(255), 
 	"Food_Storage__c" VARCHAR(255), 
 	"Name" VARCHAR(255), 
+	"Delivery_Notes__c" VARCHAR(255), 
 	"Delivery__c" VARCHAR(255), 
 	PRIMARY KEY (id)
 );
-INSERT INTO "Delivery_Item__c" VALUES(1,'2021-06-30','Non-refrigerated','Carrots','1');
-INSERT INTO "Delivery_Item__c" VALUES(2,'2021-07-08','Refrigerated','Tomatoes','1');
+INSERT INTO "Delivery_Item__c" VALUES(1,'2021-06-30','Non-refrigerated','Carrots','Wash very well','1');
+INSERT INTO "Delivery_Item__c" VALUES(2,'2021-07-08','Refrigerated','Tomatoes','','1');
 CREATE TABLE "Delivery__c" (
 	id INTEGER NOT NULL, 
 	"Schedule_Date__c" VARCHAR(255), 
